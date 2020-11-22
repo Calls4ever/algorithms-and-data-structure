477350086d538eb93db646732ce404b99c3ba7fe
callas4ever <thetseringnorbu@gmail.com>
2020-11-22 16:46:30 -0500

    linked list almost done

--- LinkedList/linkedList.js
+++ LinkedList/linkedList.js
@@ -1,6 +1,81 @@
 class Node{
     constructor(data){
         this.next=null
-        this.data
+        this.data=data
     }
 }
+class LinkedList{
+    constructor(){
+        this.head=null
+        this.length=0
+    }
+    isEmpty(){
+        return this.head==null
+    }
+
+    add(ele){
+       if(this.isEmpty()){
+           this.head = new Node(ele)
+           
+       }else{
+           let currentNode=this.head
+           while(currentNode.next){
+            currentNode=currentNode.next
+           }
+           
+           currentNode.next= new Node(ele)
+       }
+       this.length++
+    }
+    print(){
+        let currentNode=this.head, res=''
+        if(this.isEmpty()){
+            res='empty'
+        }else{
+            while(currentNode){
+            res+='data: ['+currentNode.data+']->next: '
+            currentNode=currentNode.next
+            }
+        }
+        
+        
+        console.log(res)
+    }
+    reverse(){
+        if(this.isEmpty()){
+            return null
+        }
+        let currentNode=this.head, stack=[]
+        while(currentNode){
+            stack.push(currentNode)
+            currentNode=currentNode.next
+        }
+        this.head=null
+        while(stack.length>0){
+            this.add(stack.pop().data)
+        };
+        console.log(stack)
+    }
+}
+
+let l=new LinkedList()
+l.print()
+console.log(l.isEmpty())
+l.add('tsering')
+l.add('kalsang')
+l.add('god')
+l.add('linked list')
+l.add('engineering')
+l.add('where?')
+l.add('nachos')
+l.add('Hell')
+l.add('merry')
+l.add('Holiday')
+l.add('but')
+l.add('ok')
+l.add(12)
+l.add(48)
+l.add(48)
+l.print()
+l.reverse()
+l.print()
